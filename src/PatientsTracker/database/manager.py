2359af6e0becc8a60fb903019c730a41bf1b9a88
user23 <user23@adventus.local>
2024-08-16 13:46:38 +0300

    Database + Structures

--- src/PatientsTracker/database/manager.py
+++ src/PatientsTracker/database/manager.py
@@ -1,12 +1,13 @@
-from datetime import date
+from datetime import date, datetime
 import sqlite3
-from PatientsTracker.database import constants, structures
+from PatientsTracker.database import constants
+from PatientsTracker import structures
 
 
 class DataBaseManager:
     DB_LOCATION = "./patients.db"  # TODO: Environment variable
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.connection = sqlite3.connect(self.DB_LOCATION)
         self.cursor = self.connection.cursor()
         self._create_table()
@@ -26,6 +27,9 @@ class DataBaseManager:
     def add_new_patient(self, patient_info: structures.Patient) -> None:
         self.cursor.execute(constants.INSERT_TABLE_COMMAND, patient_info.convert_to_tuple())
 
+    def get_patients_by_time(self, start_date: date, end_date: date) -> list:
+        return self.cursor.execute(constants.SELECT_PATIENTS_BY_VISIT_COMMAND, (start_date, end_date)).fetchall()
+
     def _create_table(self) -> None:
         self.cursor.execute(constants.CREATE_TABLE_COMMAND)
 
@@ -34,7 +38,9 @@ with DataBaseManager() as db:
     patient = structures.Patient(
         fullname="John",
         date_of_birth=date(year=2020, month=10, day=30),
-        date_of_visit=date(year=2000, month=10, day=30)
+        time_of_visit=datetime.now()
     )
     db.add_new_patient(patient)
-
+    # a = db.get_patients_by_time(date(year=2000, month=10, day=30), datetime.now())
+    # print(a)
+    print(datetime.now().isocalendar())
--- src/PatientsTracker/structures.py
+++ src/PatientsTracker/structures.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-from datetime import date
+from datetime import date, datetime
 
 
 @dataclass
 class Patient:
     fullname: str
     date_of_birth: date
-    date_of_visit: date
+    time_of_visit: datetime
 
     def convert_to_tuple(self) -> tuple:
-        return self.fullname, self.date_of_birth, self.date_of_visit
+        return self.fullname, self.date_of_birth, self.time_of_visit
